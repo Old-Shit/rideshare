b5e61ccb787a6975ca35a61c1c4243cccd0e243b
Frank Faustino <nawzyah@gmail.com>
2018-07-15 13:35:42 -0700

    Config

--- App.js
+++ App.js
@@ -1,23 +1,77 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { NavigatorIOS, View } from 'react-native'
+import {
+  apiKey,
+  authDomain,
+  databaseURL,
+  messagingSenderId,
+  projectId,
+  storageBucket,
+} from 'react-native-dotenv'
+import * as firebase from 'firebase'
 
-export default class App extends React.Component {
+import Destination from './components/Destination'
+import Login from './components/Login'
+import PickupLocation from './components/PickupLocation'
+
+import styles from './styles'
+
+export default class App extends Component {
+  constructor() {
+    super()
+    this.state = { layout: null }
+  }
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey,
+      authDomain,
+      databaseURL,
+      messagingSenderId,
+      projectId,
+      storageBucket,
+    })
+  }
+
+  onLayout = ({ nativeEvent: { layout } }) => {
+    this.setState({ layout })
+  }
+
+  // renderScene = (route, navigator) => {
+  //   const { layout } = this.state
+  //   const props = {
+  //     layout,
+  //     route,
+  //     navigator,
+  //   }
+
+  //   // console.log(props)
+  //   Object.assign(props, route.passProps || {})
+
+  //   switch (route.id) {
+  //     case 'Login':
+  //       return <Login {...props} />
+  //     case 'PickupLocation':
+  //       return <PickupLocation {...props} />
+  //     case 'Destination':
+  //       return <Destination {...props} />
+  //     default:
+  //       return null
+  //   }
+  // }
+
   render() {
+    const { layout } = this.state
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View onLayout={this.onLayout} style={styles.container}>
+        {layout ? (
+          <NavigatorIOS
+            initialRoute={{ component: Login, passProps: { layout }, title: 'Login' }}
+            // renderScene={this.renderScene}
+            style={styles.navigator}
+          />
+        ) : null}
       </View>
-    );
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
